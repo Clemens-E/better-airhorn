5427be335413416e5c41a5facc74c3b6bbca7132
Clemens Ertle <clemensertle@gmail.com>
2019-07-04 14:38:59 +0200

    Reworking Update command

--- cmds/update.js
+++ cmds/update.js
@@ -1,21 +1,30 @@
-const {
-    exec,
-} = require('child_process');
+const promisify = require('util').promisify;
+const exec = promisify(require('child_process').exec);
 module.exports.run = async (client, message) => {
-    if (message.author.id !== client.config.ownerid) return;
     const msg = await message.channel.send(`${client.config.updating} executing pull command...`);
-    exec('git pull origin master', async (err, stdout) => {
-        if (err) throw err;
-        if (stdout === 'Already up-to-date.\n') return msg.edit(stdout);
-        await msg.edit(`${client.config.updating}\`\`\`fix\n${stdout}\`\`\`${client.config.loading}Now restarting`);
+    exec('git pull origin master').then(async r => {
+        const stdout = r.stdout;
+        if (stdout === 'Already up to date.\n') return msg.edit(stdout);
+        if (message.author.id !== client.config.ownerid) return msg.edit('Updates available, but you don\'t have enough permissions.');
+        const raw = `\`\`\`fix\n${stdout}\`\`\``;
+        await msg.edit(`${raw}Updating dependencies ${client.config.loading}`);
+        const worked = await exec('npm i').catch(console.log);
+        if (!worked) return msg.edit(`${raw}Updating dependencies failed, restart cancelled`);
+        await msg.edit(`${raw}Dependencies updated.`);
         client.settings.set('lastMessage', {
             msg: msg.id,
             channel: msg.channel.id,
             content: stdout,
         });
+        await client.destroy();
         console.log('updated code. restarting now');
         process.exit(0);
+    }).catch(err => {
+        msg.edit('Something went wrong.');
+        console.log(err);
     });
+
+
 };
 
 exports.help = {
