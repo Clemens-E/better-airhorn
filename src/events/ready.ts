24bf969231026e978400beac0f453a236db3f08d
Clemens Ertle <clemensertle@gmail.com>
2019-09-08 14:47:33 +0200

    Posting stats to dbl

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,40 @@
 import { BClient } from '../models/Client';
 import { logger } from '../classes/Logger';
+import fetch from 'node-fetch';
 
+let counter = 0;
 module.exports = (client: BClient): void => {
     client.AudioStorage.fetchAll().then((r): void =>
         r.forEach((x): Promise<boolean> => client.AudioStorage.similarity.add(x.commandname))
     );
     logger.log(`ready as ${client.user.tag}`);
+
+
+    function updateStatus(): void {
+        const status = [`${client.guilds.size.toLocaleString()} Guilds`, 'Tag me for Info',
+        `${client.guilds.map(g => g.memberCount).reduce((a, b) => a + b).toLocaleString()} Users`,
+        `${client.channels.size.toLocaleString()} Channels`];
+        counter++;
+        if (counter === status.length) counter = 0;
+        client.user.setActivity(status[counter]);
+    }
+    function postStats(): void {
+        fetch(`https://discordbots.org/api/bots/${client.user.id}/stats`,
+            {
+                method: 'POST',
+                headers: { 'Authorization': process.env.DBLTOKEN, 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    'server_count': client.guilds.size,
+                    'shard_count': client.ws.shards.size,
+                }),
+            })
+            .then((r) => logger.debug(`posted stats ${r.status}`));
+    }
+
+    postStats();
+    updateStatus();
+    client.setInterval(postStats, 10 * 1_000 * 60);
+    client.setInterval(updateStatus, 15 * 1_000);
+
 };
+
